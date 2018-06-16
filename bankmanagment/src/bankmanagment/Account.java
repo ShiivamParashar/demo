3bf0a24196369639b348aab57d54a20dfcc2384b
Shivam <Shivam@DESKTOP-K7SENT8>
2018-06-17 00:07:40 +0530

    bank

---
+++ bankmanagment/src/bankmanagment/Account.java
@@ -0,0 +1,22 @@
+package bankmanagment;
+
+/*interface Acc{
+	String name,acc_type;
+    int Acc_num,Acc_Balance;
+	
+}*/
+
+public class Account{
+	String name,acc_type;
+    int Acc_num,Acc_Balance;
+    Account(){
+      
+    }
+        Account(String n,int acc_num,int b,String a_t){
+            name=n;
+            Acc_num=acc_num;
+            Acc_Balance=b;
+            acc_type=a_t;
+        }
+
+}
